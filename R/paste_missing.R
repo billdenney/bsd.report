6a2ccc951c5616a6e9d9d5d32242091d6c70df0f
Bill Denney <wdenney@humanpredictions.com>
2019-01-06 22:29:27 -0500

    Add paste_missing

---
+++ R/paste_missing.R
@@ -0,0 +1,48 @@
+#' Concatenate strings dropping missing values
+#'
+#' @details If all values are missing, the value from the first argument is
+#'   preserved.  \code{paste_last} affects the final output; the main difference
+#'   is that if \code{FALSE}, \code{NA_character_} values will be preserved, and
+#'   if \code{TRUE}, \code{NA_character_} values will be converted to "NA" (as
+#'   is the case with \code{paste()}).
+#'
+#' @inheritParams base::paste
+#' @param missing_values Values considered missing to be ignored in pasting.
+#' @param paste_last When all \code{...} arguments have been combined and only
+#'   one remains, should \code{paste} be called on that last argument?  (Ignored
+#'   if \code{collapse} is not \code{NULL}.)
+#' @return A character vector of pasted values.
+#' @export
+paste_missing <- function(..., sep=" ", collapse=NULL, missing_values=NA, paste_last=FALSE) {
+  args <- list(...)
+  if (length(args) <= 1) {
+    if (length(args) == 0) {
+      # match the behavior of paste
+      paste(sep=sep, collapse=collapse)
+    } else if (paste_last | !is.null(collapse)) {
+      paste(..., sep=sep, collapse=collapse)
+    } else {
+      args[[1]]
+    }
+  } else {
+    # There are at least 2 arguments; collapse the first two and recurse
+    mask1 <- !(args[[1]] %in% missing_values)
+    mask2 <- !(args[[2]] %in% missing_values)
+    mask_both <- mask1 & mask2
+    mask_only2 <- (!mask1) & mask2
+    firsttwo <- args[[1]]
+    if (any(mask_only2)) {
+      firsttwo[mask_only2] <- args[[2]][mask_only2]
+    }
+    if (any(mask_both)) {
+      # Collapse only occurs on the final pasting
+      firsttwo[mask_both] <- paste(args[[1]][mask_both], args[[2]][mask_both], sep=sep, collapse=NULL)
+    }
+    new_args <- append(list(firsttwo), args[-(1:2)])
+    new_args$sep <- sep
+    new_args$collapse <- collapse
+    new_args$missing_values <- missing_values
+    new_args$paste_last <- paste_last
+    do.call(paste_missing, new_args)
+  }
+}
---
+++ tests/testthat/test-paste_missing.R
@@ -0,0 +1,54 @@
+context("paste_missing")
+
+test_that("standard missing paste", {
+  expect_equal(
+    paste_missing(), character(0),
+    info="zero arguments matches paste()"
+  )
+  expect_equal(
+    paste_missing(c("A", "B", NA)),
+    c("A", "B", NA),
+    info="one argument is returned as-is"
+  )
+  expect_equal(
+    paste_missing(c("A", "B", NA), collapse=";"),
+    "A;B;NA",
+    info="one argument is returned as-is, collapsing"
+  )
+  expect_equal(
+    paste_missing(c("A", "B", NA), c(NA, "C", "D")),
+    c("A", "B C", "D")
+  )
+  expect_equal(
+    paste_missing(c("A", "B", NA), c(NA, "C", "D"), sep="E"),
+    c("A", "BEC", "D")
+  )
+  expect_equal(
+    paste_missing(c("A", "B", NA), c(NA, "C", "D"), sep="E", collapse=";"),
+    c("A;BEC;D")
+  )
+  expect_equal(
+    paste_missing(
+      c("A", "B", NA, NA),
+      c(NA, "C", "D", NA)
+    ),
+    c("A", "B C", "D", NA)
+  )
+  expect_equal(
+    paste_missing(
+      c("A", "B", NA, NA, NA),
+      c(NA, "C", "D", NA, NA),
+      c(NA, NA, "E", "F", NA)
+    ),
+    c("A", "B C", "D E", "F", NA)
+  )
+  expect_equal(
+    paste_missing(
+      c("A", "B", NA, NA, NA),
+      c(NA, "C", "D", NA, NA),
+      c(NA, NA, "E", "F", NA),
+      missing_values=c(NA, "F")
+    ),
+    c("A", "B C", "D E", NA, NA)
+  )
+})
