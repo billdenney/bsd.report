0a0807fab8dd9f749f88f8226a8562b2d403ec7b
Bill Denney <wdenney@humanpredictions.com>
2019-02-04 17:23:11 -0500

    Add emax and its inverse

---
+++ R/math_helpers.R
@@ -0,0 +1,22 @@
+#' Compute the Emax or its inverse.
+#'
+#' @details The equation for the Emax function is
+#'
+#'   \deqn{effect = e0 + \frac{emax \times x^{hill}}{ex50^{hill} + x^{hill}}}
+#'
+#' @param effect The effect
+#' @param e0 Baseline effect
+#' @param emax Maximum effect (at the limit as \code{x} approaches \code{Inf})
+#' @param ex50 Amount of x yielding 50% of the maximum effect
+#' @param hill The hill slope (also known as the sigmoidicity)
+#' @return \code{effect} for \code{emax_fun} or \code{x} for \code{inverse_emax}
+#' @export
+emax_fun <- function(x, e0, emax, ex50, hill=1) {
+  e0 + emax*x^hill/(ex50^hill + x^hill)
+}
+#' @describeIn emax_fun Inverse emax
+#' @export
+inverse_emax <- function(effect, e0, emax, ec50, hill=1) {
+  current_effect <- effect - e0
+  (current_effect*(ec50^hill)/(emax*(1-current_effect/emax)))^(1/hill)
+}
