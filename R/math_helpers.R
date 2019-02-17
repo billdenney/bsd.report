647dccecfb8457a3c27d65b60333bb8346b1b4ea
Bill Denney <wdenney@humanpredictions.com>
2019-02-17 14:14:33 -0500

    Add cumsum_reset

--- R/math_helpers.R
+++ R/math_helpers.R
@@ -21,3 +21,24 @@ inverse_emax <- function(effect, e0, emax, ex50, hill=1) {
   current_effect <- effect - e0
   (current_effect*(ex50^hill)/(emax*(1-current_effect/emax)))^(1/hill)
 }
+
+#' Perform a cumulative sum resetting to zero whenever a \code{reset} value is
+#' found.
+#'
+#' @param x The vector to sum over
+#' @param reset The value when found in \code{x} to reset the sum to zero at.
+#'   (Note that the value at \code{x %in% reset} is zero.
+#' @return A vector the same length as \code{x}
+#' @export
+cumsum_reset <- function(x, reset=NA) {
+  count <- 0
+  ret <- rep(NA_integer_, length(x))
+  for (i in seq_along(x)) {
+    count <- count + x[i]
+    if (x[i] %in% reset) {
+      count <- 0
+    }
+    ret[i] <- count
+  }
+  ret
+}
