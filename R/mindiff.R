801b1d0e79ab977d1ad419625985a368a4e600e4
Bill Denney <wdenney@humanpredictions.com>
2019-03-04 14:26:51 -0500

    Add mindiff

---
+++ R/mindiff.R
@@ -0,0 +1,86 @@
+#' Find the minimum difference between a vector and a set of choices.
+#'
+#' @param x The vector of values to adjust
+#' @param choices The vector of values which should be matched
+#' @param tie How should a tie (two choices are equidistant) be managed? (See
+#'   details.)
+#' @return The vector of \code{x} replaced by the closest values from the
+#'   \code{choices} vector.
+#' @seealso \code{\link{mindiff_after}}
+#' @export
+mindiff <- function(x, choices, tie=c("first", "last", "median-first", "median-last")) {
+  tie <- match.arg(tie)
+  if (length(choices) > 0) {
+    choices <- sort(choices)
+    distances <- abs(sapply(x, FUN="-", choices))
+    apply(X=distances,
+          MARGIN=2,
+          FUN=function(x, tie) {
+            ret <- which(x == min(x))
+            if (length(ret) > 1) {
+              if (tie == "first") {
+                ret <- ret[1]
+              } else if (tie == "last") {
+                ret <- ret[length(ret)]
+              } else if (tie == "median-first") {
+                ret <- ret[floor(length(ret)/2)]
+              } else if (tie == "median-last") {
+                ret <- ret[ceiling(length(ret)/2)]
+              } else {
+                stop("Invalid value for 'tie' argument")
+              }
+            }
+            choices[ret]
+          },
+          tie=tie)
+  } else {
+    stop("No choices given.")
+  }
+}
+
+#' Return the minimum of the differences of x for the first value after 
+#' the choices.
+#' 
+#' @param x The values to select choices for.
+#' @param choices The choices from which to select the minimum value.
+#' @param none What do you do if \code{x < min(choices)}?  Return a 
+#'   \code{"negative"} value or return \code{NA}?
+#' @return A vector the same length as \code{x} with the minimum 
+#'   difference between \code{x} and any value of \code{choices}.  If
+#'   \code{choices} has length 0, then return a vector of \code{NA} the
+#'   same length as \code{x} (with a warning).
+#' @seealso \code{\link{mindiff}}
+#' @export
+mindiff_after <- function(x, choices, include.zero=TRUE, none=c("negative", "na")) {
+  none <- match.arg(none)
+  if (length(choices) > 0) {
+    choices <- sort(choices)
+    distances <- sapply(x, FUN="-", choices)
+    if (is.null(nrow(distances))) {
+      # Make it a matrix
+      distances <- t(distances)
+    }
+    apply(X=distances,
+          MARGIN=2,
+          FUN=function(x, none) {
+            if (include.zero) {
+              ret <- which(x >= 0)
+            } else {
+              ret <- which(x > 0)
+            }
+            if (length(ret) > 0) {
+              min(x[ret])
+            } else if (none == "negative") {
+              max(x)
+            } else if (none == "na") {
+              NA
+            } else {
+              stop("Invalid value for 'none' argument")
+            }
+          },
+          none=none)
+  } else {
+    warning("No choices given, returning NA")
+    rep(NA, length(x))
+  }
+}
