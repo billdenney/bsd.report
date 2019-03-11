da60fb84a5bdf7fe9d24dceb513cb6d01e2aefad
Bill Denney <wdenney@humanpredictions.com>
2019-03-10 21:55:36 -0400

    add_caption

--- R/latex_helpers.R
+++ R/latex_helpers.R
@@ -27,3 +27,25 @@ latex_label <- function(x) {
 latex_label_clean <- function(x) {
   Hmisc::latexTranslate(gsub(pattern=" ", replacement="-", x=x))
 }
+
+#' Append a caption to a string vector of captions
+#'
+#' @param x The list of captions to add to.
+#' @param value a vector with length 1 or 2.  If \code{length(value) == 1}, it
+#'   is the caption text.  If \code{length(value) == 2}, the first element is
+#'   the caption text and the second is the label for the figure (to be passed
+#'   into \code{latex_label}).
+#' @return \code{c(x, paste0(latex_label(value[2]), value[1]))} or \code{c(x, value)}
+#' @export
+`add_caption<-` <- function(x, value) {
+  c(
+    x,
+    if (length(value) == 2) {
+      paste0(latex_label(value[2]), value[1])
+    } else if (length(value) == 1) {
+      value
+    } else {
+      stop("value must have length 1 or 2.")
+    }
+  )
+}
