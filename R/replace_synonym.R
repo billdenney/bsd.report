99bcbae8fbce48f4f3c2666ab6bac1a15ea83f57
Bill Denney <wdenney@humanpredictions.com>
2020-04-22 17:05:58 -0400

    Add data.frame method for replace_synonym()

--- R/replace_synonym.R
+++ R/replace_synonym.R
@@ -1,8 +1,10 @@
 #' Replace values in a character string with their synonym
 #' 
 #' @param x The values to possibly replace
-#' @param synonyms A named character vector where the names are the verbatim value and the values are the values to use for replacement.
+#' @param synonyms A named character vector where the names are the verbatim
+#'   value and the values are the values to use for replacement.
 #' @param ignore_case Should the synonyms be replaced case-insensitively?
+#' @param ... Passed to other `replace_synonym()` methods.
 #' @return `x` with preferred values instead of verbatim values.
 #' @examples 
 #' replace_synonym(
@@ -14,9 +16,30 @@
 #'   c(A="apple", B="bear", C="cabbage"),
 #'   ignore_case=FALSE
 #' )
+#' replace_synonym(
+#'   x=
+#'     data.frame(
+#'       A=rep(c("A", "B"), each=2),
+#'       B=letters[1:4],
+#'       stringsAsFactors=FALSE
+#'     ),
+#'     synonyms=
+#'       data.frame(
+#'         A="A",
+#'         Column="B",
+#'         Verbatim=c("a", "c"),
+#'         Preferred=c("apple", "cherry"),
+#'         stringsAsFactors=FALSE
+#'       )
+#' )
 #' @export
 #' @importFrom dplyr recode
-replace_synonym <- function(x, synonyms, ignore_case=TRUE) {
+replace_synonym <- function(x, synonyms, ignore_case=TRUE, ...)
+  UseMethod("replace_synonym")
+
+#' @rdname replace_synonym
+#' @export
+replace_synonym.character <- function(x, synonyms, ignore_case=TRUE, ...) {
   if (!is.character(x)) {
     stop("`x` must be a character vector")
   } else if (!is.character(synonyms)) {
@@ -36,4 +59,101 @@ replace_synonym <- function(x, synonyms, ignore_case=TRUE) {
     x[mask_replaced] <- tolower(x[mask_replaced])
   }
   do.call(dplyr::recode, append(list(.x=x), as.list(synonyms)))
+}
+
+#' @rdname replace_synonym
+#' @param replacement_column The column name in `synonyms` to find where in `x`
+#'   to apply synonyms.
+#' @param verbatim_column The column name in `synonyms` with verbatim terms.
+#' @param preferred_column The column name in `synonyms` with preferred terms.
+#' @importFrom dplyr left_join
+#' @importFrom purrr is_scalar_character
+#' @importFrom tidyr nest unnest
+#' @export
+replace_synonym.data.frame <- function(x, synonyms, ignore_case=TRUE, ...,
+                                       replacement_column="Column",
+                                       verbatim_column="Verbatim",
+                                       preferred_column="Preferred") {
+  if (!is.data.frame(synonyms)) {
+    stop("`synonyms` must be a data.frame if `x` is a data.frame.")
+  } else if (!purrr::is_scalar_character(replacement_column)) {
+    stop("`replacement_column` must be a scalar character string")
+  } else if (!purrr::is_scalar_character(verbatim_column)) {
+    stop("`verbatim_column` must be a scalar character string")
+  } else if (!purrr::is_scalar_character(preferred_column)) {
+    stop("`preferred_column` must be a scalar character string")
+  } else if (!is.character(synonyms[[replacement_column]])) {
+    stop("`synonyms[[replacement_column]]` must be a character column.")
+  } else if (!is.character(synonyms[[verbatim_column]])) {
+    stop("`synonyms[[verbatim_column]]` must be a character column.")
+  } else if (!is.character(synonyms[[preferred_column]])) {
+    stop("`synonyms[[preferred_column]]` must be a character column.")
+  }
+  if (replacement_column %in% names(x)) {
+    orig_replacement_column <- replacement_column
+    replacement_column <- paste0(max(names(x)), "X")
+    names(synonyms)[names(synonyms) %in% orig_replacement_column] <- replacement_column
+  }
+  synonym_cols <- c(replacement_column, verbatim_column, preferred_column)
+  by_cols <- setdiff(names(synonyms), synonym_cols)
+  if (length(by_cols)) {
+    if (!all(by_cols %in% names(x))) {
+      stop("All columns in `synonyms` other than the `replacement_column`, `verbatim_column`, and `preferred_column` must be names of `x`.")
+    }
+
+    synonym_nest <- tidyr::nest(synonyms, synonyms=synonym_cols)
+    x_nest <-
+      tidyr::nest(x, data=setdiff(names(x), by_cols))
+    x_synonym <- dplyr::left_join(x_nest, synonym_nest, by=by_cols)
+    x_synonym$preferred <-
+      mapply(
+        FUN=replace_synonym_single_data_frame,
+        x=x_synonym$data,
+        synonyms=x_synonym$synonyms,
+        ignore_case=ignore_case,
+        replacement_column=replacement_column,
+        verbatim_column=verbatim_column,
+        preferred_column=preferred_column,
+        SIMPLIFY=FALSE
+      )
+    x_synonym$data <- x_synonym$synonyms <- NULL
+    ret <- tidyr::unnest(data=x_synonym, cols="preferred")
+  } else {
+    ret <-
+      replace_synonym_single_data_frame(
+        x=x,
+        synonyms=synonyms,
+        ignore_case=ignore_case,
+        replacement_column=replacement_column,
+        verbatim_column=verbatim_column,
+        preferred_column=preferred_column
+      )
+  }
+  ret
+}
+
+# Helper function for replace_synonym.data.frame doing the work when a single
+# data.frame is needed.
+#' @importFrom stats setNames
+replace_synonym_single_data_frame <- function(x, synonyms, ignore_case=TRUE,
+                                              replacement_column="Column",
+                                              verbatim_column="Verbatim",
+                                              preferred_column="Preferred") {
+  if (!is.null(synonyms)) {
+    col_to_replace <- unique(synonyms[[replacement_column]])
+    for (current_col in col_to_replace) {
+      mask_synonym <- synonyms[[replacement_column]] %in% current_col
+      x[[current_col]] <-
+        replace_synonym.character(
+          x=x[[current_col]],
+          synonyms=
+            stats::setNames(
+              synonyms[[preferred_column]][mask_synonym],
+              nm=synonyms[[verbatim_column]][mask_synonym]
+            ),
+          ignore_case=ignore_case
+        )
+    }
+  }
+  x
 }
--- tests/testthat/test-replace_synonym.R
+++ tests/testthat/test-replace_synonym.R
@@ -1,6 +1,6 @@
 context("replace_synonym")
 
-test_that("replace_synonym works", {
+test_that("replace_synonym.character works", {
   expect_equal(
     replace_synonym(
       c("A", "B", "C", "a"),
@@ -27,30 +27,156 @@ test_that("replace_synonym works", {
   )
 })
 
-test_that("replace_synonym errors appropriately", {
+test_that("replace_synonym.data.frame works", {
+  expect_equal(
+    replace_synonym.data.frame(
+      x=
+        data.frame(
+          A=rep(c("A", "B"), each=2),
+          B=letters[1:4],
+          stringsAsFactors=FALSE
+        ),
+      synonyms=
+        data.frame(
+          A="A",
+          Column="B",
+          Verbatim=c("a", "c"),
+          Preferred=c("apple", "cherry"),
+          stringsAsFactors=FALSE
+        )
+    ),
+    data.frame(
+      A=rep(c("A", "B"), each=2),
+      B=c("apple", "b", "c", "d"),
+      stringsAsFactors=FALSE
+    )
+  )
+  expect_equal(
+    replace_synonym.data.frame(
+      x=
+        data.frame(
+          A=rep(c("A", "B"), each=2),
+          B=letters[1:4],
+          stringsAsFactors=FALSE
+        ),
+      synonyms=
+        data.frame(
+          A=rep(c("A", "B"), each=2),
+          Column="B",
+          Verbatim=c("a", "c", "b", "d"),
+          Preferred=c("apple", "cherry", "berry", "durian"),
+          stringsAsFactors=FALSE
+        )
+    ),
+    data.frame(
+      A=rep(c("A", "B"), each=2),
+      B=c("apple", "b", "c", "durian"),
+      stringsAsFactors=FALSE
+    )
+  )
+})
+
+test_that("replace_synonym.character errors appropriately", {
   expect_error(
-    replace_synonym(1, 1),
+    replace_synonym.character(1, 1),
     regexp="`x` must be a character vector",
     fixed=TRUE
   )
   expect_error(
-    replace_synonym("A", 1),
+    replace_synonym.character("A", 1),
     regexp="`synonyms` must be a character vector.",
     fixed=TRUE
   )
   expect_error(
-    replace_synonym("A", "A"),
+    replace_synonym.character("A", "A"),
     regexp="`synonyms` must be named.",
     fixed=TRUE
   )
   expect_error(
-    replace_synonym("A", c(a="A", "B")),
+    replace_synonym.character("A", c(a="A", "B")),
     regexp="All `synonyms` must be named, and no names may be blank.",
     fixed=TRUE
   )
   expect_error(
-    replace_synonym("A", c(a="A", a="B")),
+    replace_synonym.character("A", c(a="A", a="B")),
     regexp="All names of `synonyms` (verbatim values) must be unique.",
     fixed=TRUE
   )
+})
+
+test_that("replace_synonym.data.frame errors appropriately", {
+  expect_error(
+    replace_synonym.data.frame(1, 1),
+    regexp="`synonyms` must be a data.frame",
+    fixed=TRUE
+  )
+  expect_error(
+    replace_synonym.data.frame(
+      x=data.frame(A="A"),
+      synonyms=data.frame(Column=1, Verbatim=1, Preferred=1, stringsAsFactors=FALSE)
+    ),
+    regexp="`synonyms[[replacement_column]]` must be a character column.",
+    fixed=TRUE
+  )
+  expect_error(
+    replace_synonym.data.frame(
+      x=data.frame(A="A"),
+      synonyms=data.frame(Column="A", Verbatim=1, Preferred=1, stringsAsFactors=FALSE)
+    ),
+    regexp="`synonyms[[verbatim_column]]` must be a character column.",
+    fixed=TRUE
+  )
+  expect_error(
+    replace_synonym.data.frame(
+      x=data.frame(A="A"),
+      synonyms=data.frame(Column="A", Verbatim="A", Preferred=1, stringsAsFactors=FALSE)
+    ),
+    regexp="`synonyms[[preferred_column]]` must be a character column.",
+    fixed=TRUE
+  )
+  expect_error(
+    replace_synonym.data.frame(
+      x=data.frame(A="A"),
+      synonyms=data.frame(Column="A", Verbatim="A", Preferred="A", stringsAsFactors=FALSE)
+    ),
+    regexp="`x` must be a character vector",
+    fixed=TRUE
+  )
+  expect_error(
+    replace_synonym.data.frame(
+      x=data.frame(A="A", stringsAsFactors=FALSE),
+      synonyms=data.frame(Column="A", Verbatim="A", Preferred="A", stringsAsFactors=FALSE),
+      replacement_column=c("A", "B")
+    ),
+    regexp="`replacement_column` must be a scalar character string",
+    fixed=TRUE
+  )
+  expect_error(
+    replace_synonym.data.frame(
+      x=data.frame(A="A", stringsAsFactors=FALSE),
+      synonyms=data.frame(Column="A", Verbatim="A", Preferred="A", stringsAsFactors=FALSE),
+      verbatim_column=c("A", "B")
+    ),
+    regexp="`verbatim_column` must be a scalar character string",
+    fixed=TRUE
+  )
+  expect_error(
+    replace_synonym.data.frame(
+      x=data.frame(A="A", stringsAsFactors=FALSE),
+      synonyms=data.frame(Column="A", Verbatim="A", Preferred="A", stringsAsFactors=FALSE),
+      preferred_column=c("A", "B")
+    ),
+    regexp="`preferred_column` must be a scalar character string",
+    fixed=TRUE
+  )
+
+  expect_error(
+    replace_synonym.data.frame(
+      x=data.frame(A="A", stringsAsFactors=FALSE),
+      synonyms=data.frame(B="A", Column="A", Verbatim="A", Preferred="A", stringsAsFactors=FALSE)
+    ),
+    regexp="All columns in `synonyms` other than the `replacement_column`, `verbatim_column`, and `preferred_column` must be names of `x`.",
+    fixed=TRUE
+  )
+  
 })
