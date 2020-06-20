e58435ab39940b1b8b017ef3d4c9e7023e990078
Bill Denney <wdenney@humanpredictions.com>
2020-06-20 09:30:05 -0400

    Complete testing of fun_no_na

--- tests/testthat/test-fun_no_na.R
+++ tests/testthat/test-fun_no_na.R
@@ -8,7 +8,14 @@ test_that("fun_no_na", {
   expect_equal(max_no_na(double()), double())
   expect_equal(max_no_na(double(), zero_len=NA), NA_real_)
   expect_equal(max_no_na(character(), zero_len=NA), NA_character_)
-  
+  expect_error(
+    max_no_na(character(), zero_len="A"),
+    regexp="`zero_len` must be `NULL` or `NA`.",
+    fixed=TRUE
+  )
+
   expect_equal(min_no_na(c(1, 2, NA)), 1)
   expect_equal(min_no_na(NA), NA)
+  
+  expect_equal(median_no_na(c(1, 2, NA)), 1.5)
 })
