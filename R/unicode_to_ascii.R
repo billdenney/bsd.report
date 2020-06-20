ed7ed61b33237dd0b5eb4d8c50718926360783a8
Bill Denney <wdenney@humanpredictions.com>
2020-06-20 19:57:23 -0400

    Add unicode_to_ascii()

---
+++ R/unicode_to_ascii.R
@@ -0,0 +1,82 @@
+#' Convert from Unicode to ASCII
+#' 
+#' @param x Object that may contain character strings for conversion.
+#' @param verbose Provide messages about number of values changed.
+#' @return An object of the same class as \code{x} with Unicode characters
+#'   converted to ASCII.
+#' @export
+unicode_to_ascii <- function(x, ...) {
+  UseMethod("unicode_to_ascii")
+}
+
+#' @rdname unicode_to_ascii
+#' @export
+unicode_to_ascii.default <- function(x, verbose=FALSE, ...) {
+  if (verbose) {
+    message("Returning unchanged: Cannot convert from Unicode to ASCII for class ", class(x)[1])
+  }
+  x
+}
+
+#' @rdname unicode_to_ascii
+#' @param pattern,replacement,... Passed to \code{stringi::stri_replace_all_fixed()}
+#' @param general_conversion After the initial, specific conversions, run
+#'   \code{stringi::stri_trans_general(x,
+#'   id="Any-Latin;Greek-Latin;Latin-ASCII")} on the current strings.
+#' @export
+#' @importFrom stringi stri_replace_all_fixed
+unicode_to_ascii.character <- function(x, verbose=FALSE,
+                                       pattern=c("\u03bc", "\u00b5"),
+                                       replacement=c("u", "u"),
+                                       general_conversion=TRUE, ...) {
+  ret <-
+    stringi::stri_replace_all_fixed(
+      str=x, pattern=pattern, replacement=replacement, vectorize_all=FALSE,
+      ...
+    )
+  if (general_conversion) {
+    ret <- stringi::stri_trans_general(ret, id="Any-Latin;Greek-Latin;Latin-ASCII")
+  }
+  if (verbose) {
+    if (changes <- sum(x != ret)) {
+      message(
+        changes,
+        ngettext(
+          changes,
+          " change for Unicode to ascii conversion.",
+          " changes for Unicode to ascii conversion."
+        )
+      )
+    } else {
+      message("No Unicode to ascii changes.")
+    }
+  }
+  ret
+}
+
+#' @rdname unicode_to_ascii
+#' @export
+unicode_to_ascii.factor <- function(x, ...) {
+  levels(x) <- unicode_to_ascii(levels(x), ...)
+  x
+}
+
+#' @rdname unicode_to_ascii
+#' @export
+unicode_to_ascii.logical <-
+  unicode_to_ascii.NULL <-
+  unicode_to_ascii.numeric <-
+  function(x, verbose=FALSE, ...) {
+    if (verbose) message("No Unicode to ascii changes for class: ", class(x)[1])
+    x
+  }
+
+#' @rdname unicode_to_ascii
+#' @export
+unicode_to_ascii.data.frame <- function(x, verbose=FALSE, ...) {
+  for (idx in seq_along(x)) {
+    if (verbose) message("Unicode to ascii current column number and name: ", idx, ", ", names(x)[idx])
+    x[[idx]] <- unicode_to_ascii(x[[idx]], verbose=verbose, ...)
+  }
+  x
+}
---
+++ tests/testthat/test-unicode_to_ascii.R
@@ -0,0 +1,85 @@
+context("unicode_to_ascii")
+
+test_that("unicode_to_ascii", {
+  expect_equal(
+    expect_message(
+      unicode_to_ascii("\u03bc", verbose=TRUE),
+      regexp="1 change for Unicode to ascii conversion"
+    ),
+    "u"
+  )
+  expect_equal(
+    expect_silent(unicode_to_ascii("\u03bc", verbose=FALSE)),
+    "u"
+  )
+  expect_equal(
+    expect_silent(unicode_to_ascii("\u03bc", verbose=FALSE)),
+    "u"
+  )
+  expect_equal(
+    unicode_to_ascii("\u4E01"),
+    "ding"
+  )
+  expect_equal(
+    unicode_to_ascii("\u4E01", general_conversion=FALSE),
+    "\u4E01"
+  )
+  expect_equal(
+    unicode_to_ascii(
+      factor(c("\u4E01", "a"), levels=c("\u4E01", "a")),
+      general_conversion=FALSE
+    ),
+    factor(c("\u4E01", "a"), levels=c("\u4E01", "a"))
+  )
+  expect_equal(
+    unicode_to_ascii(
+      factor(c("\u4E01", "a"), levels=c("\u4E01", "a"))
+    ),
+    factor(c("ding", "a"), levels=c("ding", "a"))
+  )
+  expect_equal(unicode_to_ascii(NA), NA)
+  expect_equal(unicode_to_ascii(TRUE), TRUE)
+  expect_equal(
+    expect_message(
+      unicode_to_ascii(5, verbose=TRUE),
+      regexp="No Unicode to ascii changes for class: numeric",
+      fixed=TRUE
+    ),
+    5
+  )
+  expect_null(
+    expect_message(
+      unicode_to_ascii(NULL, verbose=TRUE),
+      regexp="No Unicode to ascii changes for class: NULL",
+      fixed=TRUE
+    )
+  )
+  expect_equal(
+    expect_message(
+      unicode_to_ascii(as.POSIXct("2020-06-20"), verbose=TRUE),
+      regexp="Returning unchanged.*POSIXct"
+    ),
+    as.POSIXct("2020-06-20")
+  )
+  expect_equal(
+    expect_message(
+      unicode_to_ascii(
+        data.frame(
+          Date=as.POSIXct("2020-06-20"),
+          Character=c("abc \u4E01 def", "\u4E01", "A", "\u03bc"),
+          Number=1:4,
+          stringsAsFactors=FALSE
+        ),
+        verbose=TRUE
+      ),
+      regexp="Unicode to ascii current column number and name: 2, Character",
+      fixed=TRUE
+    ),
+    data.frame(
+      Date=as.POSIXct("2020-06-20"),
+      Character=c("abc ding def", "ding", "A", "u"),
+      Number=1:4,
+      stringsAsFactors=FALSE
+    )
+  )
+})
