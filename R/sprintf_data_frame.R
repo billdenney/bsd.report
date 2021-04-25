c3aa752852a3328bc64d24c1b17c1d7a02173814
Bill Denney <wdenney@humanpredictions.com>
2021-04-25 11:32:09 -0400

    Added factor_out_if_factor_in argument to sprintf_data_frame()

--- R/sprintf_data_frame.R
+++ R/sprintf_data_frame.R
@@ -4,6 +4,11 @@
 #' @param ... a named list of character vectors.  Names are new columns for
 #'   \code{data}, and values are sent to \code{format} in
 #'   \code{sprintf_data_frame_single}.
+#' @param factor_out_if_factor_in If any of the input columns are factors, make
+#'   the output column a factor in the same order as the input column factors
+#' @param ordered If \code{factor_out_if_factor_in} converts the output to a
+#'   factor, pass to \code{base::factor}.  If \code{NULL}, then it is set to
+#'   \code{TRUE} if any of the input columns are ordered factors.
 #' @return The data frame with columns added for the names of \code{...}.
 #' @examples
 #' sprintf_data_frame(
@@ -12,7 +17,7 @@
 #'   disp_hp=c(disp="%g cu.in. displacement, ", hp="%g hp")
 #' )
 #' @export
-sprintf_data_frame <- function(data, ...) {
+sprintf_data_frame <- function(data, ..., factor_out_if_factor_in=TRUE, ordered=NULL) {
   args <- list(...)
   stopifnot("... arguments must be named"=!is.null(names(args)))
   stopifnot("All ... arguments must be named"=!any(names(args) %in% ""))
@@ -21,7 +26,9 @@ sprintf_data_frame <- function(data, ...) {
     data[[current_nm]] <-
       sprintf_data_frame_single(
         data=data,
-        format=args[[current_nm]]
+        format=args[[current_nm]],
+        factor_out_if_factor_in=factor_out_if_factor_in,
+        ordered=ordered
       )
   }
   data
@@ -39,15 +46,28 @@ sprintf_data_frame <- function(data, ...) {
 #'   format=c(mpg="%g miles/gallon, ", cyl="%g cylinders")
 #' )
 #' @export
-sprintf_data_frame_single <- function(data, format) {
+sprintf_data_frame_single <- function(data, format, factor_out_if_factor_in=TRUE, ordered=NULL) {
   stopifnot("'format' must be named"=!is.null(names(format)))
   stopifnot("All elements of 'format' must be named"=!any(names(format) %in% ""))
   stopifnot("All names of 'format' must be column headers in 'data'"=all(names(format) %in% names(data)))
-  do.call(
-    sprintf,
-    append(
-      list(fmt=paste0(format, collapse="")),
-      unname(data[, names(format), drop=FALSE])
+  ret <-
+    do.call(
+      sprintf,
+      append(
+        list(fmt=paste0(format, collapse="")),
+        unname(data[, names(format), drop=FALSE])
+      )
     )
-  )
+  if (factor_out_if_factor_in) {
+    d_inputs <- unique(data[, names(format), drop=FALSE])
+    if (any(sapply(X=d_inputs, FUN=is.factor))) {
+      if (is.null(ordered)) {
+        ordered <- any(sapply(X=d_inputs, FUN=is.ordered))
+      }
+      d_inputs$rowid <- seq_len(nrow(d_inputs))
+      d_inputs <- dplyr::arrange(.data=d_inputs, dplyr::across(.cols=everything()))
+      ret <- base::factor(ret, levels=unique(ret)[d_inputs$rowid], ordered=ordered)
+    }
+  }
+  ret
 }
--- tests/testthat/test-sprintf_data_frame.R
+++ tests/testthat/test-sprintf_data_frame.R
@@ -13,6 +13,81 @@ test_that("sprintf_data_frame_single", {
   )
 })
 
+test_that("sprintf_data_frame_single factoring", {
+  mtcars_f <- mtcars
+  mtcars_f$cyl <- factor(mtcars_f$cyl)
+  mtcars_o <- mtcars
+  mtcars_o$cyl <- ordered(mtcars_f$cyl)
+  expect_false(
+    is.factor(sprintf_data_frame_single(
+      data=mtcars,
+      format=c(mpg="%g miles/gallon, ", cyl="%g cylinders")
+    ))
+  )
+  expect_true(
+    is.factor(sprintf_data_frame_single(
+      data=mtcars_f,
+      format=c(mpg="%g miles/gallon, ", cyl="%g cylinders")
+    ))
+  )
+  expect_true(
+    is.ordered(sprintf_data_frame_single(
+      data=mtcars_o,
+      format=c(mpg="%g miles/gallon, ", cyl="%g cylinders")
+    ))
+  )
+  expect_true(
+    is.ordered(sprintf_data_frame_single(
+      data=mtcars_f,
+      format=c(mpg="%g miles/gallon, ", cyl="%g cylinders"),
+      ordered=TRUE
+    ))
+  )
+  expect_false(
+    is.ordered(sprintf_data_frame_single(
+      data=mtcars_o,
+      format=c(mpg="%g miles/gallon, ", cyl="%g cylinders"),
+      ordered=FALSE
+    ))
+  )
+  expect_equal(
+    levels(sprintf_data_frame_single(
+      data=mtcars_f,
+      format=c(mpg="%g miles/gallon, ", cyl="%g cylinders")
+    )),
+    levels(sprintf_data_frame_single(
+      data=mtcars_o,
+      format=c(mpg="%g miles/gallon, ", cyl="%g cylinders")
+    ))
+  )
+  expect_equal(
+    sprintf_data_frame_single(
+      data=mtcars_f,
+      format=c(mpg="%g miles/gallon, ", cyl="%g cylinders")
+    ),
+    structure(
+      c(17L, 17L, 21L, 19L, 13L, 12L, 3L, 22L, 21L, 14L, 
+                11L, 9L, 10L, 6L, 1L, 1L, 4L, 26L, 25L, 27L, 20L, 7L, 6L, 2L, 
+                15L, 24L, 23L, 25L, 8L, 16L, 5L, 18L),
+      .Label = c("10.4 miles/gallon, 3 cylinders", 
+                 "13.3 miles/gallon, 3 cylinders", "14.3 miles/gallon, 3 cylinders", 
+                 "14.7 miles/gallon, 3 cylinders", "15 miles/gallon, 3 cylinders", 
+                 "15.2 miles/gallon, 3 cylinders", "15.5 miles/gallon, 3 cylinders", 
+                 "15.8 miles/gallon, 3 cylinders", "16.4 miles/gallon, 3 cylinders", 
+                 "17.3 miles/gallon, 3 cylinders", "17.8 miles/gallon, 2 cylinders", 
+                 "18.1 miles/gallon, 2 cylinders", "18.7 miles/gallon, 3 cylinders", 
+                 "19.2 miles/gallon, 2 cylinders", "19.2 miles/gallon, 3 cylinders", 
+                 "19.7 miles/gallon, 2 cylinders", "21 miles/gallon, 2 cylinders", 
+                 "21.4 miles/gallon, 1 cylinders", "21.4 miles/gallon, 2 cylinders", 
+                 "21.5 miles/gallon, 1 cylinders", "22.8 miles/gallon, 1 cylinders", 
+                 "24.4 miles/gallon, 1 cylinders", "26 miles/gallon, 1 cylinders", 
+                 "27.3 miles/gallon, 1 cylinders", "30.4 miles/gallon, 1 cylinders", 
+                 "32.4 miles/gallon, 1 cylinders", "33.9 miles/gallon, 1 cylinders"
+      ), class = "factor"
+    )
+  )
+})
+
 test_that("sprintf_data_frame_single errors", {
   expect_error(
     sprintf_data_frame_single(
