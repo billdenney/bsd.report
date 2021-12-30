e360be0e04f7e7f81735b8581425a39e028abdcc
Bill Denney <wdenney@humanpredictions.com>
2021-12-30 11:29:10 -0500

    Add latex_label_first_last()

--- R/latex_helpers.R
+++ R/latex_helpers.R
@@ -27,3 +27,27 @@ latex_label <- function(x) {
 latex_label_clean <- function(x) {
   gsub(x=x, pattern="[^A-Za-z0-9:-]+", replacement="-")
 }
+
+#' @describeIn latex_reference Generate a latex label for the first and last of
+#'   a list of objects with blanks between
+#' @param object The object (with a length > 1)
+#' @param text The text to use for the start of the label (usually "fig:xxx" or
+#'   "tab:xxx")
+#' @param sep The separator to use between the text and the suffix
+#' @param suffix_text a 2-long character string with the text to use after the
+#'   separator
+#' @return A character vector with latex labels for the first and last objects
+#'   and blank strings in between.
+#' @export
+latex_label_first_last <- function(object, text, sep="-", suffix_text=c("first", "last")) {
+  stopifnot("object must be >1 long"=length(object) > 1)
+  stopifnot("text must be a scalar"=length(text) == 1)
+  stopifnot("sep must be a scalar"=length(sep) == 1)
+  stopifnot("suffix_text must be 2 long"=length(suffix_text) == 2)
+  idx_first <- 1
+  idx_last <- length(object)
+  ret <- rep("", length(object))
+  ret[idx_first] <- bsd.report::latex_label(paste(text, suffix_text[1], sep=sep))
+  ret[idx_last] <- bsd.report::latex_label(paste(text, suffix_text[2], sep=sep))
+  ret
+}
--- tests/testthat/test-latex_helpers.R
+++ tests/testthat/test-latex_helpers.R
@@ -10,3 +10,37 @@ test_that("latex_label_clean", {
   expect_equal(latex_label_clean("fig:A(B)"), "fig:A-B-")
   expect_equal(latex_label_clean("tab:A(B)"), "tab:A-B-")
 })
+
+test_that("latex_label_first_last", {
+  expect_equal(
+    latex_label_first_last(1:3, text="fig:test"),
+    c("\\label{fig:test-first}", "", "\\label{fig:test-last}")
+  )
+  expect_equal(
+    latex_label_first_last(1:2, text="fig:test"),
+    c("\\label{fig:test-first}", "\\label{fig:test-last}")
+  )
+})
+
+test_that("latex_label_first_last errors", {
+  expect_error(
+    latex_label_first_last(1, text="fig:test"),
+    regexp="object must be >1 long",
+    fixed=TRUE
+  )
+  expect_error(
+    latex_label_first_last(1:2, text=c("A", "fig:test")),
+    regexp="text must be a scalar",
+    fixed=TRUE
+  )
+  expect_error(
+    latex_label_first_last(1:2, text="fig:test", sep=c("A", "B")),
+    regexp="sep must be a scalar",
+    fixed=TRUE
+  )
+  expect_error(
+    latex_label_first_last(1:2, text="fig:test", suffix_text="foo"),
+    regexp="suffix_text must be 2 long",
+    fixed=TRUE
+  )
+})
