036dd9c7308916812a1af2beca471787cfda3b80
Bill Denney <wdenney@humanpredictions.com>
2023-12-07 04:47:45 -0500

    Soft-hyphenate GitHub SHA hashes

--- R/knit_print.sessionInfo.R
+++ R/knit_print.sessionInfo.R
@@ -1,5 +1,5 @@
 #' Print the sessionInfo in a way that is usable in knitr/rmarkdown reports
-#' 
+#'
 #' @param x The output of sessionInfo() (if missing, it is generated)
 #' @param ... Ignored
 #' @return A knitr \code{asis_output()} version of the session info.
@@ -11,7 +11,7 @@ knit_print.sessionInfo <- function(x, ...) {
   packagename <- function(x) {
     sha <- ifelse(
       "GithubSHA1" %in% names(x),
-      paste(", GithubSHA1:", x$GithubSHA1),
+      paste(", GithubSHA1:", soft_hyphenate(x$GithubSHA1)),
       ""
     )
     sprintf("%s(%s%s)", x$Package, x$Version, sha)
@@ -27,4 +27,11 @@ knit_print.sessionInfo <- function(x, ...) {
       paste("    * Attached:", paste(otherpkg, collapse="; ")),
       paste("    * Namespaces (not attached):", paste(namespacepkg, collapse="; ")))
   knitr::asis_output(paste(ret, collapse="\n"))
+}
+
+soft_hyphenate <- function(x, character_width = 8) {
+  pattern <- sprintf("(.{%d})", character_width)
+  ret <- gsub(x = x, pattern = pattern, replacement = "\\1\u00ad")
+  # Drop hyphenation at the end in case there are exactly 8 characters
+  gsub(x = ret, pattern = "\u00ad$", replacement = "")
 }
